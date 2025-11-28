89acec76884befd78396552bf3f6842477abb246
Göktuğ Berke Güngören <gungorengoktugberke@gmail.com>
2025-11-28 16:13:11 +0300

    Adds activity card component and homepage
    Introduces a reusable activity card component to display event information.
    
    Creates a homepage to showcase multiple activity cards, fetching data from a hardcoded implementation to be replaced by database data later.
    
    Implements basic styling for the card and homepage.

---
+++ sosyalizbiz-frontend/src/components/card/Card.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import './Card.css';
+interface person {
+    name: string;
+}
+interface CardProps {
+    id: string;
+    title: string;
+    person: person;
+    currentAttendees: number;
+    maxAttendees: number;
+    date: Date;
+    createdAt: Date;
+    content: string;
+    place: string;
+}
+const Card: React.FC<CardProps> = ({ id, title, content, person, currentAttendees, maxAttendees, date, createdAt, place }) => {
+    return (
+        <>
+            <div>{id}</div>
+            <div className="card" >
+                <div className="card-header">
+                    <h2>{title}</h2>
+                    <h3>{person.name}</h3>
+                </div>
+                <div className="card-content">
+                    <div className="card-element">
+                        <div className='card-label'>
+                            <h4>Kontenjan</h4>
+                        </div>
+                        <div className="card-value">
+                            <p>{currentAttendees} / {maxAttendees}</p>
+                        </div>
+                    </div>
+
+                    <div className="card-element">
+                        <div className='card-label'>
+                            <h4>Tarih</h4>
+                        </div>
+                        <div className="card-value">
+                            <p>{date.toLocaleDateString()}</p>
+                        </div>
+                    </div>
+
+                    <div className="card-element">
+                        <div className='card-label'>
+                            <h4>Açıklama</h4>
+                        </div>
+                        <div className="card-value">
+                            <p>{content}</p>
+                        </div>
+                    </div>
+
+                    <div className="card-element">
+                        <div className='card-label'>
+                            <h4>Oluşturulma Tarihi</h4>
+                        </div>
+                        <div className="card-value">
+                            <p>{createdAt.toLocaleTimeString()}</p>
+                        </div>
+                    </div>
+
+                    <div className="card-element">
+                        <div className='card-label'>
+                            <h4>Yer</h4>
+                        </div>
+                        <div className="card-value">
+                            <p>{place}</p>
+                        </div>
+                    </div>
+
+                    <div className="card-element">
+
+                        <div className='frame'>iframe</div>
+                    </div>
+
+                </div>
+                <div className="card-button" >
+                    <button>Etkinliğe Katıl</button>
+                </div>
+
+            </div>
+
+        </>
+
+    );
+};
+export default Card;
